116b9d51c7890e4c69e2c1807098b5252f41d3b0
Lucian Loghin <lucian.loghin@gmail.com>
2017-10-26 12:56:15 +0300

    Added Account class before refactoring

---
+++ OopAndPatterns/StatePattern1/AccountInitialForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopAndPatterns.StatePattern1
+{
+    class AccountInitialForm
+    {
+        public decimal Balance { get; private set; }
+
+        private Action OnUnfreeze { get; }
+        private bool IsVerified { get; set; }
+        private bool IsClosed { get; set; }
+        private bool IsFrozen { get; set; }
+
+        public AccountInitialForm(Action onUnfreeze)
+        {
+            this.OnUnfreeze = onUnfreeze;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (!this.IsVerified)
+                return;
+            if (!this.IsClosed)
+                return;
+            if (this.IsFrozen)
+            {
+                this.IsFrozen = false;
+                this.OnUnfreeze();
+            }
+
+            this.Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (!this.IsVerified)
+                return;
+            if (!this.IsClosed)
+                return;
+
+            this.Balance -= amount;
+        }
+
+        public void Close()
+        {
+            this.IsClosed = true;
+        }
+
+        public void HolderVerified()
+        {
+            IsVerified = true;
+        }
+
+        public void Freeze()
+        {
+            if (!this.IsVerified)
+                return;
+            if (!this.IsClosed)
+                return;
+
+            this.IsFrozen = true;
+        }
+    }
+}
