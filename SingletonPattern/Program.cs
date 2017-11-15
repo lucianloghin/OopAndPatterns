e91d045e120ffef7d97be238354311630bbe202c
lucianloghin <lucian.loghin@gmail.com>
2017-11-15 22:46:06 +0200

    Adde SingletonPattern project

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() => Console.WriteLine(SingletonClass.Instance.GetHashCode()));
+            Task.Factory.StartNew(() => Console.WriteLine(SingletonClass.Instance.GetHashCode()));
+            Console.ReadLine();
+            SingletonClass s = new SingletonClass();
+        }
+    }
+}
---
+++ SingletonPattern/SingletonClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace SingletonPattern
+{
+    public sealed class SingletonClass
+    {
+        [ThreadStatic]
+        private static SingletonClass _instance;
+
+        public static SingletonClass Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        static SingletonClass()
+        {
+            _instance = new SingletonClass();
+        }
+    }
+}
